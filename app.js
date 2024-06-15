8ccdccf751e6b77ffcb591c5e14f8a0997737be6
Zoll-e <debrecenizoltan19@gmail.com>
2024-06-15 13:33:21 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,114 @@
+// Observer to track changes in state
+class Observer {
+  static observers = [];
+
+  static subscribe(fn) {
+    this.observers.push(fn);
+  }
+
+  static unsubscribe(fnToRemove) {
+    this.observers = this.observers.filter((fn) => fn !== fnToRemove);
+  }
+
+  static notify(data) {
+    this.observers.forEach((fn) => fn(data));
+  }
+}
+
+// Handle changes in local storage
+class Store {
+  static initialize() {
+    if (localStorage.getItem("count") === null) {
+      localStorage.setItem("count", 0);
+    }
+  }
+
+  static saveCount(count) {
+    localStorage.setItem("count", count);
+    Observer.notify(count);
+  }
+
+  static getCount() {
+    return localStorage.getItem("count") ?? 0;
+  }
+
+  static resetCount() {
+    this.saveCount(0);
+  }
+}
+
+// Handle changes on UI
+class UI {
+  static displayCount() {
+    const count = Store.getCount();
+    document.querySelector("#count").textContent = count;
+  }
+
+  static displayResetButton() {
+    const existingResetButton = document.querySelector("#reset");
+    if (existingResetButton) return;
+
+    const button = document.createElement("a");
+    button.id = "reset";
+    button.className = "btn btn-info px-5 mx-5";
+    button.appendChild(document.createTextNode("Reset"));
+
+    const container = document.querySelector(".container");
+    const count = document.querySelector("#count");
+
+    container.insertBefore(button, count);
+    document.querySelector("#reset").addEventListener("click", () => {
+      Store.resetCount();
+    });
+  }
+
+  static hideResetButton() {
+    const resetButton = document.querySelector("#reset");
+    if (resetButton) {
+      resetButton.removeEventListener("click", Store.resetCount);
+      resetButton.remove();
+    }
+  }
+
+  static updateCount(count) {
+    document.querySelector("#count").textContent = count;
+    if (count !== 0) {
+      UI.displayResetButton();
+    } else {
+      UI.hideResetButton();
+    }
+  }
+}
+
+// Handle changes in count
+class Calculate {
+  static add() {
+    const currentCount = Store.getCount();
+    Store.saveCount(parseInt(currentCount) + 1);
+  }
+
+  static subtract() {
+    const currentCount = Store.getCount();
+    Store.saveCount(parseInt(currentCount) - 1);
+  }
+}
+
+// Subscribe UI to changes in the count
+Observer.subscribe(UI.updateCount);
+
+// Initialize app
+function initializeApp() {
+  Store.initialize();
+
+  UI.displayCount();
+
+  document.querySelector("#add").addEventListener("click", () => {
+    Calculate.add();
+  });
+
+  document.querySelector("#subtract").addEventListener("click", () => {
+    Calculate.subtract();
+  });
+}
+
+document.addEventListener("DOMContentLoaded", () => initializeApp());
---
+++ readme.md
@@ -0,0 +1,27 @@
+# Vanilla Counter! 🍦🍨
+
+## Description:
+
+This is a 1 millionth ~~*Hello World!*~~  counter app. Why did I do it then you may wonder, well it was time for me to learn framework-agnostic javascript in depth.
+
+## What did I learn:
+> State management using the observer design pattern
+
+> DOM manipulation with pure javascript
+
+> Conditional rendering
+
+> Browser Event handling
+
+> Application lifecycle
+
+> Modularization with classes
+
+---
+
+## Key takeaways for me
+
+JS frameworks solves lot of problems, but they may introduce technical overhead, looking at you react...
+
+The choice depends solely on the situation and the problem we have to tackle, like everything else in web development.
+
